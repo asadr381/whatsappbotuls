b616f5e2aaf16982ab054e1c65241dfd442fa417
asadr381 <101998038+asadr381@users.noreply.github.com>
2025-02-12 12:29:06 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -450,7 +450,7 @@ async function createTicketWithPuppeteer(senderId, ticketData) {
                 headers: {
                     "Authorization": `token 6deab0c07f750cc:588f60f1a3a5255`,
                     "Content-Type": "application/json",
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.208 Safari/537.36',
+                    'User-Agent': userAgent,
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9',
                     'Accept-Language': 'en-US,en;q=0.5',
                     "Referer": `https://ups.sowaanerp.com/`,
